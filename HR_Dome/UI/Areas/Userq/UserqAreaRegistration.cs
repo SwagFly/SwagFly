a3936989a3ffc586358677c0b88bdb3d1d8609c8
HZWGitHub <1776481650@qq.com>
2019-06-14 18:43:00 +0800

    易嘉峰挂掉了

---
+++ HR_Dome/UI/Areas/Userq/UserqAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace UI.Areas.Userq
+{
+    public class UserqAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Userq";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Userq_default",
+                "Userq/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
