11bc29e9986561dbdf17237a402a85f56524c357
SwagFly <z65454556556a@qq.com>
2019-06-14 18:04:29 +0800

    完成七层，过滤器模型，登录界面/AJAX的传递值

---
+++ HR_Dome/UI/Controllers/HR_DomeMainController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class HR_DomeMainController : Controller
+    {
+        /// <summary>
+        /// 程序入口
+        /// </summary>
+        /// <returns></returns>
+        // GET: HR_DomeMain
+        public ActionResult Login()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 验证登录/存储登录者信息
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Show() {
+            return View();
+        }
+    }
+}
