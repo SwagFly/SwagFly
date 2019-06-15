0a19744dc5cb9d0aa2e6fbf6bc9dbaf2b53b8459
SwagFly <z65454556556a@qq.com>
2019-06-15 08:51:18 +0800

    解决IOC提交问题

---
+++ HR_Dome/IOC/IocType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;//导入引用配置文件的包
+using Unity;//工具包，用于IOC容器-----？
+using Microsoft.Practices.Unity.Configuration;//?
+
+namespace IOC
+{
+    /// <summary>
+    /// 测试IOC容器层
+    /// </summary>
+    public class IocType
+    {
+        /// <summary>
+        /// 读取配置文件，并将读取的配置文件节点放入容器IOC,并返回IOC容器
+        /// </summary>
+        /// <param name="nodeName">子节点名称</param>
+        /// <returns>IOC容器</returns>
+        private static UnityContainer CreatIoc(string nodeName)
+        {
+            UnityContainer ioc = new UnityContainer();//生成容器
+            ExeConfigurationFileMap fileObject = new ExeConfigurationFileMap();//读取文件对象
+            fileObject.ExeConfigFilename = @"";//Unity配置文件路径
+            Configuration cf = ConfigurationManager.OpenMappedExeConfiguration(fileObject, ConfigurationUserLevel.None);//通过路径生成文件
+            UnityConfigurationSection cfs = (UnityConfigurationSection)cf.GetSection("unity");
+            ioc.LoadConfiguration(cfs, nodeName);
+            return ioc;
+        }
+        /// <summary>
+        /// 转换器——》将接口转换成具体的实现类
+        /// </summary>
+        /// <typeparam name="T">需要的实现类</typeparam>
+        /// <param name="nodeName">转换标记名称</param>
+        /// <param name="typeName">子节点名</param>
+        /// <returns>需要的实现类</returns>
+        public static T GetIocType<T>(string nodeName, string typeName)
+        {
+            IUnityContainer container = CreatIoc(nodeName);
+            //
+            T newClass = container.Resolve<T>(typeName);
+            return newClass;
+        }
+    }
+}
---
+++ HR_Dome/UI/Filters/LoginAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Filters
+{
+    /// <summary>
+    /// 测试过滤器
+    /// </summary>
+    public class LoginAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// 在行为方法返回后执行
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+
+        /// <summary>
+        /// 在进入方法前执行
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+
+        /// <summary>
+        /// 在进入视图之后进行
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+        }
+
+        /// <summary>
+        /// 在进入视图之前进行
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            base.OnResultExecuting(filterContext);
+        }
+
+    }
+}
