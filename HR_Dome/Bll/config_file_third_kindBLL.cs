99f7550dff6903462c2d1166099142ff88e707b6
ImageNull19990211 <2540461031@qq.com>
2019-06-17 16:17:27 +0800

    Bll接口ioc

--- HR_Dome/Bll/config_file_third_kindBLL.cs
+++ HR_Dome/Bll/config_file_third_kindBLL.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IBll;
+using Models;
+using Dao;
+using IDao;
+using IOC;
 namespace Bll
 {
     /// <summary>
@@ -11,5 +15,6 @@ namespace Bll
     /// </summary>
     public class config_file_third_kindBLL: config_file_third_kindIBLL
     {
+        private static config_file_third_kindIDao dao = IocType.GetIocType<config_file_third_kindDao>("config_file_third_kindDao", "config_file_third_kindDao");
     }
 }
